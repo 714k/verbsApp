a9f7ba0760e898922f38da56ff2e943b4827e207
VictorZamudio <victor.zamudio@hotmail.com>
2017-11-04 19:22:15 -0600

    NAVIGATION | Navigation approach changed - The navigation approach was changed for modify title window dynamically

--- src/app/nav/nav.service.ts
+++ src/app/nav/nav.service.ts
@@ -1,43 +1,53 @@
-import { Injectable } from '@angular/core';
-import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
-import { Title } from '@angular/platform-browser';
-import { Observable } from 'rxjs';
-import { Subject } from 'rxjs/Subject';
 import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/mergeMap';
+
+import { Observable } from 'rxjs';
+import { Subject } from 'rxjs/Subject';
+import { Injectable, OnInit } from '@angular/core';
+import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
+import { Title } from '@angular/platform-browser';
 
 export class MenuItem {
-	path: string;
-	title: string;
-	icon?: string;
+  path: string;
+  title: string;
+  icon?: string;
 }
 
 @Injectable()
 export class NavService {
-	activeMenuItem$: Observable<MenuItem>;
+  activeMenuItem$: Observable<MenuItem>;
   private subject = new Subject<any>();
 
-  constructor(private router: Router, private titleService: Title) {
-  	this.activeMenuItem$ = this.router.events
-  		.filter(e => e instanceof NavigationEnd)
-  		.map(_ => this.router.routerState.root)
-  		.map(route => {
-  			let active = this.lastRouteWithMenuItem(route.root);
-  			this.titleService.setTitle(active.title);
-  			return active;
-  		});
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private titleService: Title
+  ) {
+
+    this.router.events
+      .filter((event) => event instanceof NavigationEnd)
+      .map(() => this.activatedRoute)
+      .map((route) => {
+        while (route.firstChild) route = route.firstChild;
+        return route;
+      })
+      .filter((route) => route.outlet === 'primary')
+      .mergeMap((route) => route.data)
+      .subscribe((event) => this.titleService.setTitle(event['title'] + ' | VerbsApp'));
+
   }
 
   getMenuItems(): MenuItem[] {
-  	return this.router.config
-  		.filter(route => route.data && route.data.title)
-  		.map(route => {
-  			return {
-  				path: route.path,
-  				title: route.data.title,
-  				icon: route.data.icon
-  			}
-  		});
+    return this.router.config
+      .filter(route => route.data && route.data.title)
+      .map(route => {
+        return {
+          path: route.path,
+          title: route.data.title,
+          icon: route.data.icon
+        }
+      });
   }
 
   sendCurrentSection(section: string) {
@@ -50,21 +60,6 @@ export class NavService {
 
   clearCurrentSection() {
     this.subject.next();
-  }
-
-
-  private lastRouteWithMenuItem(route: ActivatedRoute): MenuItem {
-  	let lastMenu = undefined;
-  	do { lastMenu = this.extractMenu(route) || lastMenu }
-  	while ((route = route.firstChild));
-  	return lastMenu;
-  }
-
-  private extractMenu(route: ActivatedRoute): MenuItem {
-  	let cfg = route.routeConfig;
-  	return cfg && cfg.data && cfg.data.title
-  		? { path: cfg.path, title: cfg.data.title, icon: cfg.data.icon }
-  		: undefined;
   }
 
 }
